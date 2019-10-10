922c7271c079473d514dba83371099dcf85f5a3d
thonghp166 <thonghp11@gmail.com>
2019-10-10 12:37:15 +0700

    merge and redesign architecture

--- group2_listen_english/listen_english/test/widget_test.dart
+++ group2_listen_english/listen_english/test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:listen_english/main.dart';
-import 'package:listen_english/src/screen/HomeScreen.dart';
+import 'package:listen_english/src/screen/views/HomeScreen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
